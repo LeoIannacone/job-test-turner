ed705432c8448c482732cf7dc1b158c446abd692
Leo Iannacone <info@leoiannacone.com>
2016-02-15 22:13:27 +0100

    Use autoprefix

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ import express from 'express'
 import path from 'path'
 import logger from 'morgan'
 import favicon from 'serve-favicon'
+import autoprefixer from 'express-autoprefixer'
+import ReactViews from 'express-react-views'
+import less from 'less-middleware'
 
 import routes from './routes'
+import config from './config.json'
 
 const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'js')
-app.engine('js', require('express-react-views').createEngine())
+app.engine('js', ReactViews.createEngine())
 
-// uncomment after placing your favicon in /public
 app.use(logger('dev'))
-app.use(require('less-middleware')(path.join(__dirname, 'public')))
+
+// Auto prefix CSS
+app.use(less(path.join(__dirname, 'public')))
+app.use(autoprefixer({browsers: config.supported_browsers, cascade: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
 
